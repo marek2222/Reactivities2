564876e78866237ac7ce0367c6605c84baa3d2a7
marek2222 <marekmusial2@o2.pl>
2019-12-11 13:03:58 +0100

    Reactivities2: Apl 2.07.   Reviewing the Project files

--- Application/Class1.cs
+++ Application/Class1.cs
@@ -2,7 +2,7 @@
 
 namespace Application
 {
-    public class Class1
-    {
-    }
+  public class Class1
+  {
+  }
 }
--- Persistance/Class1.cs
+++ Persistance/Class1.cs
@@ -2,7 +2,7 @@
 
 namespace Persistance
 {
-    public class Class1
-    {
-    }
+  public class Class1
+  {
+  }
 }
