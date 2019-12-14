a447432b16e25a93e2a062b7b515dd0e615b074d
marek2222 <marekmusial2@o2.pl>
2019-12-14 16:34:42 +0100

    Reactivities2: Apl 3.32.  Adding Semantic UI to our app

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,37 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Header, Icon, List } from "semantic-ui-react";
 import axios from "axios";
+import "./App.css";
 
 export default class App extends Component {
-  state = {
-    values: []
-  }
+	state = {
+		values: []
+	};
 
-  componentDidMount(){
-    axios.get('http://localhost:5000/api/values')
-      .then((response) => {
-        console.log(response);
-        this.setState({
-          values: response.data
-        });
-      })
-  }
+	componentDidMount() {
+		axios.get("http://localhost:5000/api/values").then(response => {
+			console.log(response);
+			this.setState({
+				values: response.data
+			});
+		});
+	}
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <ul>
-            {this.state.values.map((value: any) => (
-              <li key={value.id}>{value.name}</li>
-            ))}
-          </ul>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<Header as="h2">
+					<Icon name="users" />
+					<Header.Content>Reacivities2</Header.Content>
+					<List>
+						{this.state.values.map((value: any) => (
+							<List.Item key={value.id}>{value.name}</List.Item>
+						))}
+					</List>
+					<ul></ul>
+				</Header>
+			</div>
+		);
+	}
 }
-
-
