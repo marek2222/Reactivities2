c2f4be2c169d29c68cea13dfd70837c37a972489
marek2222 <marekmusial2@o2.pl>
2020-02-07 16:06:26 +0100

    Reactivities: API 5.52.  Getting a list of activities from the API

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -4,14 +4,14 @@ import axios from "axios";
 
 export default class App extends Component {
 	state = {
-		values: []
+		activities: []
 	};
 
 	componentDidMount() {
-		axios.get("http://localhost:5000/api/values").then(response => {
+		axios.get("http://localhost:5000/api/activities").then(response => {
 			console.log(response);
 			this.setState({
-				values: response.data
+				activities: response.data
 			});
 		});
 	}
@@ -21,10 +21,10 @@ export default class App extends Component {
 			<div>
 				<Header as="h2">
 					<Icon name="users" />
-					<Header.Content>Reacivities</Header.Content>
+					<Header.Content>Reactivities</Header.Content>
 					<List>
-						{this.state.values.map((value: any) => (
-							<List.Item key={value.id}>{value.name}</List.Item>
+						{this.state.activities.map((activity: any) => (
+							<List.Item key={activity.id}>{activity.title}</List.Item>
 						))}
 					</List>
 					<ul></ul>
